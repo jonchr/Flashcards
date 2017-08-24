b656763e6581eb19298ba1e090d36424ccb54478
Jon <jonathan.lee.christensen@gmail.com>
2017-08-23 21:53:38 -0400

    finished questions

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 //information is stored in object format: basicText, clozeText, answer 
 
-exports.history = [{
+exports.American_History = [{
     basicText: "Who was the commander of the Continental Army during the American Revolution?",
     clozeText: "George Washington was the commander of the Continental Army during the American Revolution.",
     answer: "George Washington"
@@ -42,15 +42,170 @@ exports.history = [{
     answer: "Crispus Attucks"
 }];
 
+exports.Math = [{
+    basicText: "What is 1 + 1?",
+    clozeText: "1 + 1 is 2",
+    answer: "2"
+}, {
+    basicText: "What is the mathematical ratio of a circle's circumference to its diameter?",
+    clozeText: "Pi is a irrational number roughly equivalent to 22/7.",
+    answer: "Pi"
+}, {
+    basicText: "What is the quadratic equation?",
+    clozeText: "The quadratic equation is x = (-b +/- SQRT(b^2-4ac))/(2a)",
+    answer: "x = (-b +/- SQRT(b^2-4ac))/(2a)"
+}, {
+    basicText: "What is Fermat's last theorem?",
+    clozeText: "Fermat's last theorem states that no three positive integers, a, b, and c, will satisfy the equation a^n + b^n = c^n for n > 2.",
+    answer: "no three positive integers, a, b, and c, will satisfy the equation a^n + b^n = c^n for n > 2."
+}, {
+    basicText: "What is the ratio of the hypotenuse over the adjacent side known as in trigonometry?",
+    clozeText: "Secant is the ratio of the hypotenus over the adjacent side of a right triangle.",
+    answer: "Secant"
+}, {
+    basicText: "What is the limit as x -> 0 of the natural log of X?",
+    clozeText: "The limit of x -> 0 of the natural log of X is Negative infinity.",
+    answer: "Negative infinity."
+}, {
+    basicText: "In the equation 7a + 3 = b, what is the value of 7 is known as?",
+    clozeText: "In the equation 7a + 3 = b, the value of 7 is known as the coefficient.",
+    answer: "coefficient."
+}, {
+    basicText: "Simplify the following expression: (x+4) * (2x+5).",
+    clozeText: "The expression  (x+4) * (2x+5) can be simplified to 2x^2 + 13x + 20.",
+    answer: "2x^2 + 13x + 20"
+}, {
+    basicText: "What is a three-dimensional object made up of 6 faces where all sides are the same?",
+    clozeText: "A three-dimensional object bounded by six square faces is known as A cube.",
+    answer: "A cube."
+}, {
+    basicText: "What is the one number that when multiplied by any real number still remains the same?",
+    clozeText: "When zero is multiplied by any real number, the answer is still Zero.",
+    answer: "Zero"
+}];
 
-exports.math = [];
+exports.English = [{
+    basicText: "What is the case of the word 'Whom'?",
+    clozeText: "The word 'Whom' is known as the objective form of the word 'Who'.",
+    answer: "objective"
+}, {
+    basicText: "What is the term for a verb that functions as a noun?",
+    clozeText: "The form of a verb when it functions as a noun is known as A gerund.",
+    answer: "A gerund"
+}, {
+    basicText: "What is a person, place, or thing?",
+    clozeText: "A noun is commonly described as a person, place, or thing.",
+    answer: "A noun"
+}, {
+    basicText: "What is the term for a part of speech that connects words, phrases, or clauses?",
+    clozeText: "In grammar, A conjunction is a part of speech that connects words, phrases, or clauses.",
+    answer: "A conjunction"
+}, {
+    basicText: "Who was the author of Dracula?",
+    clozeText: "Bram Stoker was the author of Dracula.",
+    answer: "Bram Stoker"
+}, {
+    basicText: "Which famous writer and dramatist was married to Anne Hathaway?",
+    clozeText: "William Shakespeare is widely regarded as the greatest writer in the English language and the world's pre-eminent dramatist.",
+    answer: "William Shakespeare"
+}, {
+    basicText: "What word earned Bryan Cranston his induction into the Screen Actors Guild?",
+    clozeText: "Bryan Cranston's only word in To Race the Wind was 'hike', which earned him induction into the Screen Actors Guild.",
+    answer: "hike"
+}, {
+    basicText: "What type of verb does not allow a direct object?",
+    clozeText: "In grammar, an intransitive verb does not allow a direct object.",
+    answer: "intransitive"
+}, {
+    basicText: "What is the term for a group of crows?",
+    clozeText: "A murder is the term for a group of crows.",
+    answer: "A murder"
+}, {
+    basicText: "Who wrote The Odyssey?",
+    clozeText: "Homer is credited as the author of The Odyssey.",
+    answer: "Homer"
+}]
 
+exports.Science = [{
+    basicText: "What is the boiling point of water at sea level in Fahrenheit?",
+    clozeText: "The boiling point of water at sea level is 32 degrees Fahrenheit.",
+    answer: "32 degrees"
+}, {
+    basicText: "What object in space has an icy core with a tail of gas and dust that extend for millions of miles?",
+    clozeText: "A comet is an object in space that has an icy core with a tail of gas and dust that extend for millions of miles.",
+    answer: "A comet"
+}, {
+    basicText: "What kind of waves are used to make and receive cellphone calls?",
+    clozeText: "Radio waves are the type of waves used to make and receive cellphone calls.",
+    answer: "Radio waves"
+}, {
+    basicText: "What does a light-year measure?",
+    clozeText: "A light-year is a measure of distance.",
+    answer: "distance"
+}, {
+    basicText: "What property of sound determines its loudness?",
+    clozeText: "The loudness of sound is determined by its amplitude.",
+    answer: "amplitude"
+}, {
+    basicText: "What is the rearmost lobe in the cerebral cortex?",
+    clozeText: "The occipital lobe is the rearmost of the four lobes in the cerebral cortex.",
+    answer: "The occipital lobe"
+}, {
+    basicText: "What is the most energetic galactic events known to man?",
+    clozeText: "Gamma ray bursts are the most energetic galatic events known to man.",
+    answer: "Gamma ray bursts"
+}, {
+    basicText: "What is the density of water in the metric system?",
+    clozeText: "The density of water in the metric system is 1 gram per cubic centimeter.",
+    answer: "1 gram per cubic centimeter"
+}, {
+    basicText: "In chemistry, what is a substance that aids chemical reactions without being consumed?",
+    clozeText: "A catalyst is a substance that aids in a chemical reaction without being consumed.",
+    answer: "A catalyst"
+}, {
+    basicText: "In physics, what does the constant c represent?",
+    clozeText: "In physics, the speed of light is represented by the constant c.",
+    answer: "the speed of light"
+}];
 
-
-
-exports.english = [];
-
-
-
-
-exports.science = [];
+exports.Archer = [{
+    basicText: "What is Krieger's first name?",
+    clozeText: "Dr. Kriger's first name is eventually revealed as Algernop later in the series.",
+    answer: "Algernop"
+}, {
+    basicText: "What word does Archer use to convey the letter M via the NATO phonetic alphabet?",
+    clozeText: "M as in Mancy?",
+    answer: "Mancy"
+}, {
+    basicText: "What type of party was being held at Malory's apartment in Lo Scandalo?",
+    clozeText: "In Lo Scandalo, Malory held An elegant dinner party to distract from the scene of the crime.",
+    answer: "An elegant dinner party"
+}, {
+    basicText: "What street racing name is Pam known by the Yakuza",
+    clozeText: "Pam is called by the street racing name Shiro Kabocha, or White Pumpkin, by the Yakuza.",
+    answer: "Shiro Kabocha, or White Pumpkin"
+}, {
+    basicText: "What is Cheryl's pet and potentially Archer's spirit animal?",
+    clozeText: "Cheryl's pet and potentially Archer's spirit animal is an ocelot.",
+    answer: "an ocelot"
+}, {
+    basicText: "What are we going to do with literally, not figuratively, a ton of cocaine?",
+    clozeText: "When Archer and company have literally, not figurately, a ton of cocaine, they decide to Form a cartel.",
+    answer: "Form a cartel"
+}, {
+    basicText: "In The Archer Sanction, Archer recalls that the target is from a country that was what?",
+    clozeText: "In The Archer Sanction, Archer recalls that the target is from a country that was An axis power.",
+    answer: "An axis power"
+}, {
+    basicText: "What did Pam want more of when she was stuck in the elevator in the episode 'Vision Quest'?",
+    clozeText: "In the episode 'Vision Quest', Pam wanted more bear claw when she was stuck in the elevator.",
+    answer: "bear claw"
+}, {
+    basicText: "What item of clothing is Cyril Figgis most commonly seen wearing?",
+    clozeText: "Cyril Figgis is most commonly seen wearing A sweater vest.",
+    answer: "A sweater vest"
+}, {
+    basicText: "What is the name of Lana's daughter?",
+    clozeText: "Lana's daughter is named Abbiejean.",
+    answer: "Abbiejean"
+}];
